69ba547c8133ebb216a799cfc8e89bedeee0c53b
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-02-15 14:46:36 +0700

    change api
    change from usd to bAHT

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -10,8 +10,7 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-      .then(res => {
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=THB&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
         setCoins(res.data);
         console.log(res.data);
       })

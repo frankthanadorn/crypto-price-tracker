f12bac64c2b06c9f3c66b32184109c0d8e80d2b3
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-02-15 14:13:42 +0700

    installing axios

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+
+  const [coins,setCoins] = useState([]);
+
+  useEffect(() => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  .then(res =>{
+    setCoins(res.data);
+    console.log(res.data);
+  })
+  .catch(error => console.log(error));
+}, []);
+
+
+
   return (
-    <div className="App">
-      <h1>API</h1>
-    </div>
+    <div className='coin-app'>
+    <div className='coin-search'>
+      <h1 className='coin-text'>Search a currency</h1>
+      <form>
+          <input type ='text'
+          placeholder='Search'
+          className='coin-input'
+            
+          />
+        </form>
+      </div>
+      </div>
   );
 }
 

0a16455ec649bee9df2a7a75b32dda467e97f30b
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-02-15 14:50:03 +0700

    Revert "Merge pull request #1 from frankthanadorn/THB"
    This reverts commit a5fa04e6c02a71efc78debe229c9cc4646950bf4, reversing
    changes made to 6e0ada9727681c1271050dca9f11545d4f200f8d.

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -10,7 +10,8 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=THB&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
         setCoins(res.data);
         console.log(res.data);
       })
